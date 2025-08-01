6ac50e62368f3ffe0423a38459b0d6fe5694f604
StockRoomRat <13stck@gmail.com>
2025-08-01 19:31:07 +0900

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript -->
+<script src="//stockroomratcom.local/wp-content/themes/myBlog/vendor/jquery/jquery.min.js"></script>
+<script src="//stockroomratcom.local/wp-content/themes/myBlog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="//stockroomratcom.local/wp-content/themes/myBlog/js/clean-blog.min.js"></script>
+
+<?php wp_footer(); ?>
