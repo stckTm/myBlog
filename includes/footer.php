b5256704f9016f21347853c9bd9364778a5002f2
StockRoomRat <13stck@gmail.com>
2025-08-11 19:51:13 +0900

    ナビゲーションメニュー実装

--- includes/footer.php
+++ includes/footer.php
@@ -28,7 +28,7 @@
                         </a>
                     </li>
                 </ul>
-                <p class="copyright text-muted">Copyright &copy; Your Website 2020</p>
+                <p class="copyright text-muted">Copyright &copy; Stock Room Rat 2025</p>
             </div>
         </div>
     </div>
--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
@@ -12,25 +12,17 @@
         $menu_name = 'global_nav';
         if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
             $menu = wp_get_nav_menu_object($locations[$menu_name]);
+            $menu_items = wp_get_nav_menu_items($menu->term_id);
         };
-        var_dump($menu);
-        $menu_items = wp_get_nav_menu_items($menu->$term_id);
-        var_dump($menu_items);
+
         ?>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="post.html">Sample Post</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
-                </li>
+                <?php foreach ($menu_items as $item): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo esc_attr($item->url); ?>"><?php echo esc_html($item->title); ?></a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
